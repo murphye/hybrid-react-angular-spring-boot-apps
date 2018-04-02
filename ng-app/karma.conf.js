f278200fd985c6d3f6aacd5aa844f8121acc3b3d
Eric Murphy <murphye@gmail.com>
2018-04-02 10:41:59 -0400

    PhantomJS

--- ng-app/karma.conf.js
+++ ng-app/karma.conf.js
@@ -8,6 +8,7 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
@@ -15,6 +16,8 @@ module.exports = function (config) {
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+    browsers: ['PhantomJS'],
+    singleRun: true,
     coverageIstanbulReporter: {
       reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
